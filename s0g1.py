5e96e7e28b8915ee798a403fa4db5fffc9fabe91
bloo <nexushellstein@gmail.com>
2024-12-28 02:55:53 +0800

    cleaned code for s0g1

--- s0g1.py
+++ s0g1.py
@@ -1,3 +1,4 @@
+# S0 - 1st gen    |    Circle Sprite
 # The first sprite in the encyclopedia
 # fields: name, color, radius, x, y, vx, vy
 
@@ -6,18 +7,18 @@ import sys
 
 pygame.init()
 
-screen_width = 800
-screen_height = 600
-white = (255, 255, 255)
+#environment fields
+screen_size = [800, 600]
+bg_color = (255, 255, 255)
 
-screen = pygame.display.set_mode((screen_width, screen_height))
-pygame.display.set_caption("Sprite Movement")
-
+#sprite fields
 circle_color = (0, 0, 0)
 circle_radius = 20
-circle_pos = [screen_width // 2, screen_height // 2]
-
+circle_pos = [screen_size[0] // 2, screen_size[1] // 2]
 speed = 5
+
+screen = pygame.display.set_mode((screen_size[0], screen_size[1]))
+pygame.display.set_caption("Circle Sprite: S0 - 1st gen")
 
 running = True
 while running:
@@ -29,14 +30,14 @@ while running:
     keys = pygame.key.get_pressed()
     if keys[pygame.K_LEFT] and circle_pos[0] - circle_radius > 0:
         circle_pos[0] -= speed
-    if keys[pygame.K_RIGHT] and circle_pos[0] + circle_radius < screen_width:
+    if keys[pygame.K_RIGHT] and circle_pos[0] + circle_radius < screen_size[0]:
         circle_pos[0] += speed
     if keys[pygame.K_UP] and circle_pos[1] - circle_radius > 0:
         circle_pos[1] -= speed
-    if keys[pygame.K_DOWN] and circle_pos[1] + circle_radius < screen_height:
+    if keys[pygame.K_DOWN] and circle_pos[1] + circle_radius < screen_size[1]:
         circle_pos[1] += speed
 
-    screen.fill(white)
+    screen.fill(bg_color)
     pygame.draw.circle(screen, circle_color, circle_pos, circle_radius)
     pygame.display.flip()
     pygame.time.Clock().tick(60)
